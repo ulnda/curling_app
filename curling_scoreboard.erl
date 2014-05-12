10edbebaf006cabb72096fbb20151626933fbbdc
Denis Nagibin <denis.nagibin@gmail.com>
2014-05-12 13:30:28 +0400

    Add basic functionality

---
+++ curling_scoreboard.erl
@@ -0,0 +1,34 @@
+-module(curling_scoreboard).
+-compile(export_all).
+
+-behaviour(gen_event).
+
+init([]) ->
+  { ok, [] }.
+
+handle_event({ set_teams, TeamA, TeamB }, State) ->
+	curling_scoreboard_hw:set_teams(TeamA, TeamB),
+  { ok, State };
+
+handle_event({ add_points, Team, N }, State) ->
+	[curling_scoreboard_hw:add_point(Team) || _ <- lists:seq(1, N)],
+  { ok, State }; 
+
+handle_event(next_round, State) ->
+	curling_scoreboard_hw:next_round(),
+	{ ok, State }; 
+
+handle_event(_, State) ->
+	{ ok, State }.
+
+handle_call(_, State) ->
+  { ok, ok, State }.
+
+handle_info(_, State) ->
+  { ok, State }.
+
+code_change(_OldVsn, State, _Extra) ->
+  { ok, State }.
+
+terminate(_Reason, _State) ->
+  ok.
