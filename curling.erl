ce1379e8a9a8376c1a561b9944876a9aa00bb0f5
Denis Nagibin <denis.nagibin@gmail.com>
2014-05-12 14:50:40 +0400

    Add handlers for game info

--- curling.erl
+++ curling.erl
@@ -2,27 +2,32 @@
 -compile(export_all).
 
 start_link(TeamA, TeamB) ->
-    { ok, Pid } = gen_event:start_link(),
-    gen_event:add_handler(Pid, curling_scoreboard, []),
-    set_teams(Pid, TeamA, TeamB),
-    { ok, Pid }.
+  { ok, Pid } = gen_event:start_link(),
+  gen_event:add_handler(Pid, curling_scoreboard, []),
+  gen_event:add_handler(Pid, curling_accumulator, []),
+  set_teams(Pid, TeamA, TeamB),
+  { ok, Pid }.
 
 set_teams(Pid, TeamA, TeamB) ->
-    gen_event:notify(Pid, { set_teams, TeamA, TeamB }).
+  gen_event:notify(Pid, { set_teams, TeamA, TeamB }).
 
 add_points(Pid, Team, N) ->
-    gen_event:notify(Pid, { add_points, Team, N }).
+  gen_event:notify(Pid, { add_points, Team, N }).
 
 next_round(Pid) ->
-    gen_event:notify(Pid, next_round).
+  gen_event:notify(Pid, next_round).
 
 %% Subscribes the pid ToPid to the event feed.
 %% The specific event handler for the newsfeed is
 %% returned in case someone wants to leave
 join_feed(Pid, ToPid) -> 
-	HandlerId = { curling_feed, make_ref() },
-	gen_event:add_handler(Pid, HandlerId, [ToPid]),
-	HandlerId.
+  HandlerId = { curling_feed, make_ref() },
+  gen_event:add_handler(Pid, HandlerId, [ToPid]),
+  HandlerId.
  
 leave_feed(Pid, HandlerId) ->
-	gen_event:delete_handler(Pid, HandlerId, leave_feed).
+  gen_event:delete_handler(Pid, HandlerId, leave_feed).
+
+%% Returns the current game state.
+game_info(Pid) ->
+  gen_event:call(Pid, curling_accumulator, game_data).
